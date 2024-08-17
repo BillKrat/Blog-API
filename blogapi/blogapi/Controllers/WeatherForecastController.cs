0efc02cd7453788dfe07125a8049524a7ec06b46
Bill Kratochvil <53179043+BillKrat@users.noreply.github.com>
2024-08-17 08:31:25 -0500

    Created controller

---
+++ blogapi/blogapi/Controllers/WeatherForecastController.cs
@@ -0,0 +1,25 @@
+public class WeatherForecastController{
+    public WeatherForecastController() { }    
+
+    public dynamic Get(){
+        var summaries = new[]
+        {
+            "Here I is","Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        var forecast =  Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+        
+        return forecast;
+    }    
+    record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+    {
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+    }
+}
