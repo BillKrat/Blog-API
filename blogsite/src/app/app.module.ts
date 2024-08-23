c0335742ad546a021797c94c55b19f9d108a9718
Bill Kratochvil <53179043+BillKrat@users.noreply.github.com>
2024-08-22 21:14:43 -0500

    Authentication updates

--- blogsite/src/app/app.module.ts
+++ blogsite/src/app/app.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import { AuthModule, AuthClientConfig, provideAuth0 } from '@auth0/auth0-angular';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AuthButtonComponent } from './auth.button.component';
+import { CommonModule } from '@angular/common';
+import { environment as env } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -10,9 +14,14 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    AuthButtonComponent,
+    CommonModule
   ],
-  providers: [],
+  providers: [
+    provideAuth0({...env.auth})
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
