5162813a2c3b1aa7669707ab9910054acdb1ab91
Bill Kratochvil <53179043+BillKrat@users.noreply.github.com>
2024-08-24 22:47:19 -0500

    auth0 working

--- blogsite/src/app/app.component.ts
+++ blogsite/src/app/app.component.ts
@@ -1,18 +1,19 @@
 import { HttpClient} from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { UserProfileComponent } from './components/user/app.user.profile';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  providers:[UserProfileComponent]
 })
 export class AppComponent implements OnInit{
   public title = 'blogsite';
   public errorMessage:any;
   public successMessage:any;
-
+ 
   constructor(private http: HttpClient){
-
   }
 
   ngOnInit(): void {
