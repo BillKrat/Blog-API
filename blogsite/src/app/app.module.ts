5eb760829e60774474b88517e62dc4bc4826c5e0
Bill Kratochvil <53179043+BillKrat@users.noreply.github.com>
2024-08-24 21:25:00 -0500

    minor cleanup

--- blogsite/src/app/app.module.ts
+++ blogsite/src/app/app.module.ts
@@ -5,7 +5,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AuthButtonComponent } from './components/security/auth.button.component';
 import { CommonModule } from '@angular/common';
 import { environment as env } from '../environments/environment.dev';
-import { UserProfileComponent } from './components/security/app.user.profile';
+import { UserProfileComponent } from './components/user/app.user.profile';
 import { provideAuth0 } from '@auth0/auth0-angular';
 import { inject, NgModule } from '@angular/core';
 import { HttpEvent, HttpHandlerFn, HttpRequest, provideHttpClient, withInterceptors } from '@angular/common/http';
